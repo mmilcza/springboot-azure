0a84bc5749f957a9d8d6888daf024d16d05445e3
Marcio Milczanowski <mmilcza@hotmail.com>
2021-02-23 17:39:00 -0300

    Aceleracao Avanade-DIO

--- src/main/java/com/azure/springboot/service/DummyDataService.java
+++ src/main/java/com/azure/springboot/service/DummyDataService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import javax.annotation.PostConstruct;
 import java.util.UUID;
 
 @Service
